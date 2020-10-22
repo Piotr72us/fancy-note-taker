6b2475be602792fdff94a032138c2971edc48c66
Piotr Dudek <piotr72@gmail.com>
2020-10-21 23:13:30 -0400

    save before deploying to heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,10 @@ var path = require("path");
 
 module.exports = function(app) {
 
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../data/index.html"));
+    });
+
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../data/notes.html"));
     });
